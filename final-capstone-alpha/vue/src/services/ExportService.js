5e08aa7ed0d774b141c703993668d21c2dc1e329
Yukon Colegrove-Winkfield <1003597ywinkfield@gmail.com>
2023-12-14 22:05:03 -0500

    final capstone removal

--- final-capstone-alpha/vue/src/services/ExportService.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default {
-    
-  export(user) {
-    return axios.post('/export', user)
-  },
-}
--- final-capstone-alpha/vue/src/services/GenerateService.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-export default {
-    generateSequences(sessionToken, id) {
-        return axios.patch(`/generator/${id}`, {headers: {Authorization: `Bearer ${sessionToken}`}})
-    }
-}
